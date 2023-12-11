a5764518fbb9d54c54f3ce86819ed16ef23db425
nhu <scorpion.metal.sw@gmail.com>
2023-12-11 15:51:03 +0700

    feat: creatUser API

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,4 +9,71 @@ module.exports = {
       res.status(200).json({ allUser });
     }
   },
+  updateUser: async function (req, res, next) {
+    const { name, age, phone, email, key } = req.body || null;
+    const chekAll =
+      checkName(name) != null ||
+      checkEmail(email) != null ||
+      checkAge(age) != null ||
+      checkPhone(phone) != null ||
+      isEmpty(key);
+    if (chekAll) {
+      res
+        .status(400)
+        .json({ status: false, err: "Error updating information" });
+    } else {
+      const findUserNeedUpdate = await UserModel.findOneAndUpdate(
+        { email: key },
+        {
+          name,
+          age,
+          phone,
+          email,
+        }
+      );
+      if (!findUserNeedUpdate) {
+        res.status(400).json({
+          status: false,
+          err: "Could not find the user whose information needs updating",
+        });
+      } else {
+        res.status(200).json({ status: true, mess: "Update successful" });
+      }
+    }
+  },
+  createUser: async function (req, res, next) {
+    const { name, age, phone, email } = req.body || null;
+    const chekAll =
+      checkName(name) != null ||
+      checkEmail(email) != null ||
+      checkAge(age) != null ||
+      checkPhone(phone) != null;
+    if (chekAll) {
+      res.status(400).json({
+        status: false,
+        err: "Wrong information cannot create an account",
+      });
+    } else {
+      const isEmailExist = await UserModel.findOne({ email });
+      if (isEmailExist) {
+        res.status(400).json({
+          status: false,
+          err: "This email has been previously registered, please choose another email",
+        });
+      } else {
+        const createUser = await UserModel.create({
+          name,
+          age,
+          phone,
+          email,
+        });
+        if (!createUser)
+          res.status(400).json({
+            status: false,
+            err: "An error occurred during account creation",
+          });
+        res.status(200).json({ status: true, mess: "Account successfully created" });
+      }
+    }
+  },
 };
