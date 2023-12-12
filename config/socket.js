035f8be17a57586c990450cdbf9c9b8d0a6c34a5
King Nguyen <NguyenQuocVuong-git>
2023-12-12 17:14:46 +0700

    feat: config socket, handle service socket

---
+++ config/socket.js
@@ -0,0 +1,41 @@
+const socketIO = require("socket.io");
+
+const createSocketIO = (httpServer) => {
+  const io = socketIO(httpServer, {
+    allowEIO3: true,
+    cors: {
+      origin: true,
+      credentials: true,
+    },
+  });
+
+  io.on('connect', (socket) => {
+    let users = [];
+    for (let [id, s] of io.of("/").sockets) {
+      if (id !== socket.id) {
+        const user = {
+          socketId: id,
+          username: s.handshake.auth.username,
+        };
+        users.push(user);
+      }
+    }
+    const user = {
+      socketId: socket.id,
+      username: socket.handshake.auth.username,
+    }
+    console.log(user);
+    socket.emit('USER_INFO', user)
+    socket.emit("FRIENDS_LIST", users);
+    socket.broadcast.emit("FRIEND_CONNECTED", user);
+    socket.on("PRIVATE_MESSAGE", ({ from, content, to }) => {
+      io.to(to).emit("PRIVATE_MESSAGE", {
+        from,
+        content,
+        to,
+      });
+    });
+  })
+};
+
+module.exports = createSocketIO;
