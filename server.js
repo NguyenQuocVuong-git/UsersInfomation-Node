2f0a8cbb2e571ae2022b711b39832f324fd0ae75
nhu <scorpion.metal.sw@gmail.com>
2023-12-12 14:45:14 +0700

    fix: cors

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
-const userRouter = require("./router/userRouter.js")
+const express = require("express");
+const bodyParser = require("body-parser");
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
+const userRouter = require("./router/userRouter.js");
 const app = express();
-
+const cors = require("cors");
 mongoose.Promise = global.Promise;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+  })
+);
 
-app.use("/user",userRouter)
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Databse Connected Successfully!!");    
-}).catch(err => {
-    console.log('Could not connect to the database', err);
+app.use("/user", userRouter);
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Databse Connected Successfully!!");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database", err);
     process.exit();
-});
+  });
 
-app.get('/', (req, res) => {
-    res.json({"message": "Hello Crud Node Express"});
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Crud Node Express" });
 });
 
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+  console.log("Server is listening on port 3000");
 });
